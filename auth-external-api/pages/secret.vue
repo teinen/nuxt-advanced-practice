5a515d02cc400a7a7078fe6c38bdfe9b4e68cc78
諦念 <teinen.github@gmail.com>
2018-08-26 23:03:15 +0900

    Add secret page

---
+++ auth-external-api/pages/secret.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>This is secret page.</h1>
+    <nuxt-link to="/">Back to home page.</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'authenticated'
+}
+</script>
