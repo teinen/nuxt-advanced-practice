a06ca9da3e6ae20d2f53bad3e8f46708f43f6ec7
諦念 <teinen.github@gmail.com>
2018-08-26 23:36:47 +0900

    Add increment button pages

---
+++ vuex-store/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>
+      <button @click="increment">{{ counter }}</button>
+      <nuxt-link to="/about">About</nuxt-link>
+    </p>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  // fetch(context) is called by the server-side
+  // and nuxt before instantiating the component
+  fetch ({ store }) {
+    store.commit('increment')
+  },
+  computed: mapState([
+    'counter'
+  ]),
+  methods: {
+    increment () {
+      this.$store.commit('increment')
+    }
+  }
+}
+</script>
