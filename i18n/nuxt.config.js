48086b0a8a6fa1d3588fe59906d983b33bc34f22
諦念 <teinen.github@gmail.com>
2018-08-27 07:36:31 +0900

    Add package.json and nuxt.config.js

---
+++ i18n/nuxt.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  loading: { color: 'cyan' },
+  router: {
+    middleware: 'i18n'
+  },
+  plugins: ['~/plugins/i18n.js'],
+  generate: {
+    routes: [
+      '/',
+      '/about',
+      '/fr',
+      '/fr/about',
+    ]
+  }
+}
